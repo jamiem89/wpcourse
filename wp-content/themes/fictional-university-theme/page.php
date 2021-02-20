d6d18eba5141a54c6559da44f54a862491b96000
Jamie Moore <jamiemoore2689@gmail.com>
2021-02-20 18:41:36 +0000

    hooked up page nav

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -5,6 +5,8 @@
     while(have_posts()) {
         the_post();?>
 
+<!-- Page banner -->
+
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>);"></div>
     <div class="page-banner__content container container--narrow">
@@ -17,17 +19,53 @@
 
   <div class="container container--narrow page-section">
 
-    <!-- <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div> -->
+  <!-- Breadcrumb area -->
 
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
+  <?php
+  $theParent = wp_get_post_parent_id(get_the_ID());
+  if($theParent) { ?>
+    <?php echo $theParent; ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?></a> <span class="metabox__main"><?php the_title();?></span></p>
+        </div>
+
+    <?php } ?>
+
+    <!-- Section links -->
+
+    <?php
+    // If page ID has children, return array of pages.
+    // If the page ID has no children, returns null.
+    // Use this to check if page is parent or not.
+    $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+    ));
+
+    if($theParent or $testArray) { ?>
+
+        <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent);?>"><?php echo get_the_title($theParent);?></a></h2>
+        <ul class="min-list">
+            <?php
+
+                if($theParent) {
+                    $findChildrenOf = $theParent;
+                } else {
+                    $findChildrenOf = get_the_ID();
+                }
+
+                // associative array - items in the array have associated data
+                wp_list_pages(array(
+                    'title_li' => null,
+                    'child_of' => $findChildrenOf,
+                ));
+            ?>
+        </ul>
+        </div>
+
+    <?php } ?>
+
+    <!-- Content output -->
 
     <div class="generic-content">
       <?php the_content();?>
