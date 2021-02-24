97ebb8f4f8c1f88526561d4bd14c4701228c4c1a
Jamie Moore <jamiemoore2689@gmail.com>
2021-02-24 12:00:51 +0000

    allowed archive on event post type

--- wp-content/mu-plugins/post-types.php
+++ wp-content/mu-plugins/post-types.php
@@ -5,15 +5,18 @@
 function custom_post_types() {
     register_post_type('event', array(
         'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'events'),
         'menu_icon' => 'dashicons-calendar-alt',
         'labels' => array(
             'name' => 'Events',
             'add_new_item' => 'Add new event',
             'edit_item' => 'Edit event',
             'all_items' => 'All events',
-            'singular_name' => 'Event'
+            'singular_name' => 'Event',
         ),
     ));
 }
 
 add_action('init', 'custom_post_types');
+
