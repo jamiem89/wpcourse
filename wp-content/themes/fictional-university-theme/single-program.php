1c6d00db0bbce00801badc8fb084241b284ce496
Jamie Moore <jamiemoore2689@gmail.com>
2021-03-04 17:41:04 +0000

    pull out related professors on program page

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -25,6 +25,97 @@ while(have_posts()) {
             <?php the_content();?>
         </div>
 
+
+
+        <?php
+
+        $relatedProfessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'professor',
+            'order' => 'ASC',
+            'orderby' => 'title',
+            'meta_query' => array(
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . get_the_ID() . '"',
+                ),
+            ),
+
+        ));
+
+        if($relatedProfessors->have_posts()) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">' . get_the_title() .' professors</h2>';
+
+            while($relatedProfessors->have_posts()) {
+                $relatedProfessors->the_post();?>
+
+                <li><a href="<?php the_permalink();?>"><?php the_title();?></a></li>
+
+        <?php }
+
+        }
+
+        wp_reset_postdata();
+
+            $homepageEvents = new WP_Query(array(
+                'posts_per_page' => 2,
+                // -1 displays ALL posts that match
+                // 'posts_per_page' => -1,
+                'post_type' => 'event',
+                'order' => 'ASC',
+                'meta_key' => 'event_date',
+                'orderby' => 'meta_value_num',
+                'meta_query' => array(
+                    array(
+                        'key' => 'event_date',
+                        'compare' => '>=',
+                        'value' => date('Ymd'),
+                        'type' => 'numeric'
+                    ),
+                    array(
+                        'key' => 'related_programs',
+                        'compare' => 'LIKE',
+                        'value' => '"' . get_the_ID() . '"',
+                    ),
+                ),
+
+            ));
+
+            if($homepageEvents->have_posts()) {
+                echo '<hr class="section-break">';
+                echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() .' events</h2>';
+
+                while($homepageEvents->have_posts()) {
+                    $homepageEvents->the_post();?>
+
+                    <div class="event-summary">
+                        <a class="event-summary__date t-center" href="<?php the_permalink();?>">
+                            <span class="event-summary__month"><?php
+                                $eventDate = new DateTime(get_field('event_date'));
+                                echo $eventDate -> format('M');
+                            ?></span>
+                            <span class="event-summary__day"><?php echo $eventDate -> format('d');?></span>
+                        </a>
+                        <div class="event-summary__content">
+                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
+                            <p>
+                            <?php if(has_excerpt()) {
+                                echo get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 20);
+                            } ?>
+                            <a href="<?php the_permalink();?>" class="nu gray">Learn more</a></p>
+                        </div>
+                    </div>
+
+            <?php }
+
+            }
+
+        ?>
+
     </div>
 
 <?php } ?>
